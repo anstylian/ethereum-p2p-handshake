b756c7af2a52a17968c5bf330da8bbcd0091eb69
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-07-10 11:14:51 +0300

    enhance flake

--- flake.nix
+++ flake.nix
@@ -9,22 +9,75 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk {
-        };
+        naersk-lib = pkgs.callPackage naersk { };
       in rec {
         packages = {
           ethereum-p2p-handshake = naersk-lib.buildPackage {
             src = ./.;
             doDoc = true;
             doDocFail = true;
+            copyLibs = true;
           };
 
-          default = packages.ethereum-p2p-handshake;
+          handshake = naersk-lib.buildPackage {
+            pname = "handshake";
+            src = ./.;
+
+            overrideMain = old: {
+              preConfigure = ''
+                cargo_build_options="$cargo_build_options --bin handshake"
+              '';
+            };
+          };
+
+          ping-pong = naersk-lib.buildPackage {
+            pname = "ping-pong";
+            src = ./.;
+
+            overrideMain = old: {
+              preConfigure = ''
+                cargo_build_options="$cargo_build_options --bin ping-pong"
+              '';
+            };
+          };
+
+          default = naersk-lib.buildPackage {
+            src = ./.;
+
+            overrideMain = old: {
+              preConfigure = ''
+                cargo_build_options="$cargo_build_options --bins"
+              '';
+            };
+          };
+
         };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        checks = {
+          check = naersk-lib.buildPackage {
+            src = ./.;
+            mode = "check";
+          };
+
+          test = naersk-lib.buildPackage {
+            src = ./.;
+            mode = "test";
+          };
+
+          clippy = naersk-lib.buildPackage {
+            src = ./.;
+            mode = "clippy";
+            preConfigure = ''
+              cargo_build_options="$cargo_build_options --all"
+            '';
+          };
+        };
+
+        devShells = {
+          default = with pkgs; mkShell {
+            buildInputs = [ cargo rustc rustfmt rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
         };
       }
     );
